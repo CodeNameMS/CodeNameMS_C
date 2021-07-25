4fc59b5a977e3d1a6f179c96f85b2215c069bec6
CodeNameMS <86137847+CodeNameMS@users.noreply.github.com>
2021-07-25 22:43:14 +0900

    ch09.c
    C 공부 및 Test 7일차

---
+++ ch09_if-else-Quiz.c
@@ -0,0 +1,38 @@
+// ch09_if-else-Quiz
+
+#include <stdio.h>
+
+int main() {
+
+	//// 세 과목의 점수를 사용해서 합격, 불합격을 알려주는 코드 작성
+	//// - 합격 조건 : 각 과목별 점수 40점 이상, 평균 60 이상
+
+	//int math, kor, eng, avg=0;
+	//printf("수학 점수 입력 : "); scanf_s("%d", &math);
+	//printf("국어 점수 입력 : "); scanf_s("%d", &kor);
+	//printf("영어 점수 입력 : "); scanf_s("%d", &eng);
+	//avg = (math + kor + eng)/3;
+	//if ((math >= 40)&&(kor >= 40)&&(eng >= 40) && (avg >= 60))
+	//	printf("합격\n");
+	//else
+	//	printf("불합격\n");
+
+	// ATM기 코드를 작성
+	// - 현재 통장 잔액을 설정
+	//	 출금하는 금액 입력
+	//	 출금이 가능하면 출금 후 남은 잔액 통지
+	//	 > 출금 조건 : 인출 금액은 10000원 단위만 가능(1001원, 11000원 X)
+	//				   인출 금액은 잔액보다 많을 수 없습니다.
+
+	// int money_total,money_back,money_total2;
+	// printf("현재 통장 잔액 설정 : "); scanf_s("%d", &money_total);
+	// printf("출금 할 금액 입력 : "); scanf_s("%d", &money_back);
+
+	// if ((money_back >= 10000) && (money_back%10000 == 0)) {
+	//	 money_total2 = money_total - money_back;
+	//	 printf("통장 잔고 : %d", money_total2);
+	// }
+	// else
+	// 	 printf("통장 잔액이 모자라거나 10000원 단위가 아닙니다!");
+}
+
